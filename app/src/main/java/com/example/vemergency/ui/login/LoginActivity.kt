4611a4d9450ec67c951641163019c34798af6a40
kiluss2 <kiluss2@gmail.com>
2022-09-28 22:19:34 +0700

    push code

--- app/src/main/java/com/example/vemergency/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/vemergency/ui/login/LoginActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.example.vemergency.databinding.ActivityLoginBinding
 import com.google.android.material.tabs.TabLayoutMediator
-import com.kiluss.bookrate.adapter.LoginPagerAdapter
 
 class LoginActivity : AppCompatActivity() {
 
--- app/src/main/java/com/example/vemergency/ui/login/LoginPagerAdapter.kt
+++ app/src/main/java/com/example/vemergency/ui/login/LoginPagerAdapter.kt
@@ -1,10 +1,8 @@
-package com.kiluss.bookrate.adapter
+package com.example.vemergency.ui.login
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.example.vemergency.ui.login.LoginFragment
-import com.example.vemergency.ui.login.SignupFragment
 
 class LoginPagerAdapter(fm: FragmentActivity) : FragmentStateAdapter(fm) {
     enum class TabLayoutEnum {
