d3357325c88a581cc687a950556b97c33ae05090
kiluss2 <kiluss2@gmail.com>
2022-10-17 23:13:46 +0700

    push

--- app/src/main/java/com/kiluss/vemergency/data/model/Shop.kt
+++ app/src/main/java/com/kiluss/vemergency/data/model/Shop.kt
@@ -6,12 +6,13 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class Shop(
     var name: String? = null,
+    var owner: String? = null,
     var address: String? = null,
     var phone: String? = null,
     var openTime: String? = null,
     var website: String? = null,
+    var pricePerKilometer: Double? = null,
     var avgStar: Float? = null,
     var review: ArrayList<Review>? = null,
-    var user: String? = null,
     var location: LatLng? = null
 ) : Parcelable
